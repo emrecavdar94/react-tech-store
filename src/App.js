dcc8906ea058ebeeca00e3d6742b4f6f1460928e
emrecavdar <emre.cavdar@cloudnesil.com>
2019-11-24 14:35:37 +0300

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { FaHome } from 'react-icons/fa'
 export default class App extends Component {
   render() {
     return (
-      <h1>hello from tech store</h1>
+      <h1>hello from tech store <FaHome /> </h1>
     )
   }
 }
